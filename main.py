05ffe929c2f0f5917e95eacd8398c47d88e67bc6
Opxster <97250050+Opxster@users.noreply.github.com>
2022-01-07 14:08:29 +0000

    Update main.py
    removed useless code

--- main.py
+++ main.py
@@ -5,18 +5,42 @@ import win32gui
 import ch
 import time
 import html
+import textwrap
+global chatstore
+
+class bot(ch.RoomManager):
+   def onMessage(self, room, user, message):
+     global chatstore 
+     ctext = "{0}: {1}".format(user.name.title(), message.body)
+     #shift everything in chat table to make room for new message   
+     chatstore[0] = chatstore[1]
+     chatstore[1] = chatstore[2]
+     chatstore[2] = chatstore[3]
+     chatstore[3] = ctext
+     #refresh screen         
+     screen.fill(fuchsia)
+     x = 0
+     #draw everything in chat table
+     while x < 4:
+      textsurface = myfont.render(html.unescape(chatstore[x]), False, (0, 255, 0))
+      screen.blit(textsurface,(0, 50*x))
+     
+      pygame.display.update()
+      x = x +1
+
+   #every bot tick update the GUI
+   def _tick(self):
+    pygame.display.update()
+    pygame.event.get()
+
 
 pygame.init()
 pygame.font.init()
 myfont = pygame.font.SysFont('Arial Bold', 50)
 pygame.display.set_caption('ghetto ass overlay')
-global chatcount
-global chatstore
 chatstore = ["apple", "banana", "cherry", "other thing"]
-chatcount = 0 #message counter 
 
-screen = pygame.display.set_mode((800, 600)) # For borderless, use pygame.NOFRAME
-done = False
+screen = pygame.display.set_mode((800, 600))#, pygame.NOFRAME) 
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (139, 0, 0)
 screen.fill(fuchsia)  # Transparent background
@@ -29,45 +53,8 @@ win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
 win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 600, 300, 0, 0, win32con.SWP_NOSIZE)
 
-class bot(ch.RoomManager):
-   def onMessage(self, room, user, message):
-     global chatcount
-     global chatstore 
-     ctext = "{0}: {1}".format(user.name.title(), message.body)
-     #shift everything in chat table to make room for new message   
-     print(ctext)
-     chatstore[0] = chatstore[1]
-     chatstore[1] = chatstore[2]
-     chatstore[2] = chatstore[3]
-     chatstore[3] = ctext
-             
-     screen.fill(fuchsia)
-     x = 0
-     #draw everything in chat table
-     while x < 4:
-      print(x)
-      textsurface = myfont.render(html.unescape(chatstore[x]), False, (0, 255, 0))
-      screen.blit(textsurface,(0, 50*x))
-      pygame.display.update()
-      x = x +1
-
-   #every bot tick update the GUI
-   def _tick(self):
-    now = time.time()
-    for task in set(self._tasks):
-      if task.target <= now:
-        task.func(*task.args, **task.kw)
-        if task.isInterval:
-          task.target = now + task.timeout
-        else:
-          self._tasks.remove(task)
-    pygame.display.update()
-    pygame.event.get()
    
-    
-
-   
-rooms = ["vidyatendency"]
+rooms = ["testingvtbot"]
 username = "botstero"
 password = "123qwe"
 bot.easy_start(rooms,username,password)
