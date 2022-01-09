307c08aa6b1f630731e1f21d8d23f91f4a3bf4d5
celizan <cjelizan@gmail.com>
2022-01-09 00:31:52 -0800

    Add text wrapping to messages
    Iterative code that prints out the message line by line

--- pygme.py
+++ pygme.py
@@ -3,21 +3,43 @@ import win32api
 import win32con
 import win32gui
 import ch
-import tkinter as tk
-import multiprocessing
 import time
 import html
 
+
+class Message:
+    def __init__(self, msg_username, message, name_color, msg_color):
+        self.username = msg_username
+        self.message = message
+        self.nameColor = name_color
+        self.msgColor = msg_color
+
+font_size = 50
+overlay_height = 600
+overlay_width = 800
+
 pygame.init()
 pygame.font.init()
-myfont = pygame.font.SysFont('Arial Bold', 50)
+myfont = pygame.font.SysFont('Arial Bold', font_size)
 pygame.display.set_caption('ghetto ass overlay')
 global chatcount
 global chatstore
-chatstore = ["apple", "banana", "cherry", "other thing"]
-chatcount = 0  # message counter
 
-screen = pygame.display.set_mode((800, 600))  # For borderless, use pygame.NOFRAME
+chatstore = [
+    Message("test", "1", (0, 255, 0), (0, 255, 0)),
+    Message("tes", "2", (0, 255, 0), (0, 255, 0)),
+    Message("te", "3", (0, 255, 0), (0, 255, 0)),
+    Message("t", "4", (0, 255, 0), (0, 255, 0))
+]
+chatcount = 0  # message counter
+message_color = (255, 255, 255)
+shadow_color = (128, 128, 128)
+shadow_y_offset = 2
+shadow_x_offset = 1
+font_size = 50
+
+screen = pygame.display.set_mode((overlay_width, overlay_height))  # For borderless, use pygame.NOFRAME
+
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (139, 0, 0)
@@ -42,17 +64,51 @@ class bot(ch.RoomManager):
         chatstore[0] = chatstore[1]
         chatstore[1] = chatstore[2]
         chatstore[2] = chatstore[3]
-        chatstore[3] = ctext
+        chatstore[3] = Message(
+            html.unescape(user.name.title()),
+            html.unescape(message.body),
+            self.convert_chatango_colors(message.nameColor),
+            message_color
+        )
 
         screen.fill(fuchsia)
         x = 0
+        messageIndex = 0
         # draw everything in chat table
-        while x < 4:
-            print(x)
-            textsurface = myfont.render(html.unescape(chatstore[x]), False, (0, 255, 0))
-            screen.blit(textsurface, (0, 50 * x))
+        while messageIndex < len(chatstore):
+            current_message = chatstore[messageIndex]
+            messageIndex += 1
+            name_surface = myfont.render(current_message.username + ": ", False, (0, 255, 0))
+            screen.blit(name_surface, (0, font_size * x))
+
+            leftover_message = current_message.message
+            messageBounds = pygame.Rect(
+                name_surface.get_width(),
+                font_size * x,
+                overlay_width - name_surface.get_width(),
+                overlay_height - font_size * x,
+            )
+            i = 1
+            y = messageBounds.top
+            lineSpacing = -2
+            while leftover_message:
+                while myfont.size(leftover_message[:i])[0] < messageBounds.width and i < len(leftover_message):
+                    i += 1
+
+                # if we've wrapped the text, then adjust the wrap to the last word
+                if i < len(leftover_message):
+                    i = leftover_message.rfind(" ", 0, i) + 1
+
+                image = myfont.render(leftover_message[:i], False, shadow_color)
+                screen.blit(image, (messageBounds.left + shadow_x_offset, y + shadow_y_offset))
+                image = myfont.render(leftover_message[:i], False, message_color)
+                screen.blit(image, (messageBounds.left, y))
+                y += font_size + lineSpacing
+
+                leftover_message = leftover_message[i:]
+                x = x + 1
+
             pygame.display.update()
-            x = x + 1
 
     # every bot tick update the GUI
     def _tick(self):
@@ -66,6 +122,23 @@ class bot(ch.RoomManager):
                     self._tasks.remove(task)
         pygame.display.update()
         pygame.event.get()
+
+    @staticmethod
+    def convert_chatango_colors(color):
+        colorR, colorG, colorB = 0, 0, 0
+
+        if len(color) == 3:
+            colorR = int(str(color[0]) + str(color[0]), 16)
+            colorG = int(str(color[1]) + str(color[1]), 16)
+            colorB = int(str(color[2]) + str(color[2]), 16)
+        if len(color) == 6:
+            colorR = int(str(color[0:2]), 16)
+            colorG = int(str(color[2:4]), 16)
+            colorB = int(str(color[4:6]), 16)
+
+        if colorR == 0 and colorG == 0 and colorB == 0:
+            return 255, 255, 255
+        return colorR, colorG, colorB
 
 
 rooms = ["vidyatendency"]
